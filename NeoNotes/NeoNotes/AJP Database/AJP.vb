b2e828684159080895b24e5bd8c3b05d1cfba528
Aaron Campf <aaroncampf@gmail.com>
2017-02-19 16:43:03 -0800

    now use NeoNotes.txt from Dropbox

--- NeoNotes/NeoNotes/AJP Database/AJP.vb
+++ NeoNotes/NeoNotes/AJP Database/AJP.vb
@@ -1,33 +1,27 @@
-﻿Imports AppLimit.CloudComputing.SharpBox
-
-''' <summary>Contains specific functionality for AJP Northwest</summary>
+﻿''' <summary>Contains specific functionality for AJP Northwest</summary>
 Public NotInheritable Class AJP
 	Private Sub New()
 	End Sub
 
-	Private Shared Sub DownloadDatabase()
-		Dim dropBoxStorage As New CloudStorage()
-		Dim dropBoxConfig = CloudStorage.GetCloudConfigurationEasy(nSupportedCloudConfigurations.DropBox)
-		Dim accessToken As ICloudStorageAccessToken
+	'TODO: Upgrade this to grab one of the XML files that saves the table INVMA
+	Public Shared Function Get_Item_Names() As String()
+		Dim LocalFilePath = AppDomain.CurrentDomain.GetData("DataDirectory") + "\NeoNotes.txt"
+		If My.Computer.Network.IsAvailable AndAlso My.Settings.LastUpdated < Now.AddDays(-7) OrElse Not IO.File.Exists(LocalFilePath) Then
+			Dim Dbox As New Dropbox.Api.DropboxClient(My.Resources.Dropbox_AccessToken)
+			Dim File = Dbox.Files.DownloadAsync("/Storage/NeoNotes.txt").Result.GetContentAsByteArrayAsync().Result
 
-		'load a valid security token from file
-		Dim byt As Byte() = Text.Encoding.UTF8.GetBytes(My.Resources.DropBox_Token)
-		accessToken = dropBoxStorage.DeserializeSecurityTokenFromBase64(Convert.ToBase64String(byt))
 
-		'open the connection
-		Dim storageToken = dropBoxStorage.Open(dropBoxConfig, accessToken)
-		dropBoxStorage.DownloadFile("/Storage/AJP_ANS_Cache.sdf", AppDomain.CurrentDomain.GetData("DataDirectory"))
+			If IO.File.Exists(LocalFilePath) Then
+				IO.File.Delete(LocalFilePath)
+			End If
 
-		My.Settings.LastUpdated = Now
-		My.Settings.Save() '<--- Do I need this?
-	End Sub
+			IO.File.WriteAllBytes(LocalFilePath, File)
 
-	Public Shared Function Get_Item_Names() As List(Of String)
-		If My.Computer.Network.IsAvailable And My.Settings.LastUpdated < Now.AddDays(-7) Then
-			DownloadDatabase()
+			My.Settings.LastUpdated = Now
+			My.Settings.Save() '<--- Do I need this?
 		End If
 
-		Return Aggregate x In New AJP_DB().AJP_INVMAs Select x.DESCRIP Into ToList
+		Return IO.File.ReadAllLines(LocalFilePath)
 	End Function
 
 End Class
